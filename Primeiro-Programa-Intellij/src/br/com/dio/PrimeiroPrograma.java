c3a37e54b85e1d049c1d09422a0228ff9f97aee6
AlexandreCasta <castanheiraalexandre7@gmail.com>
2021-08-13 17:00:19 -0300

    First Commit Intellij

---
+++ Primeiro-Programa-Intellij/src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+
+
+        /*int a = 3;
+        int b = 2;
+        System.out.println("Dá-lhe Tricolor!!!" + a + "X" + b);*/
+
+
+    }
+}
